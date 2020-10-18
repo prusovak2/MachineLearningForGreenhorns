75aa9a5e8224b783aa459d50ad81b0730303238d
prusovak2 <prusova.k2@seznam.cz>
2020-10-18 11:00:50 +0200

    linear_regression_manual

--- labs/01/linear_regression_manual.py
+++ labs/01/linear_regression_manual.py
@@ -5,6 +5,9 @@ import numpy as np
 import sklearn.datasets
 import sklearn.metrics
 import sklearn.model_selection
+from pprint import pprint
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
 
 parser = argparse.ArgumentParser()
 # These arguments will be set appropriately by ReCodEx, even if you change them.
@@ -23,18 +26,37 @@ def main(args):
     # print(dataset.DESCR)
 
     # TODO: Append a new feature to all input data, with value "1"
+    num_rows, num_column = dataset.data.shape
+    ones_column = np.ones((num_rows, 1), dtype='int64')
+    featuresWithOnes = np.append(dataset.data, ones_column, axis=1)
 
     # TODO: Split the dataset into a train set and a test set.
     # Use `sklearn.model_selection.train_test_split` method call, passing
     # arguments `test_size=args.test_size, random_state=args.seed`.
+    x_train, x_test, y_train, y_test = train_test_split(featuresWithOnes, dataset.target, test_size=args.test_size,
+                                                        random_state=args.seed)
 
     # TODO: Solve the linear regression using the algorithm from the lecture,
     # explicitly computing the matrix inverse (using `np.linalg.inv`).
+    x_train_transpose = x_train.transpose()
+    xTx = x_train_transpose@x_train
+    # pprint(xTx)
+    inverse = np.linalg.inv(xTx)
+    # pprint(inverse)
+    weights = inverse @ x_train_transpose @ y_train
+    #pprint(y_train.shape)
 
     # TODO: Predict target values on the test set
+    # x_test_transpose = x_test.transpose()
+    # weights_transposed = weights.transpose()
+    pprint(x_test.shape)
+    # pprint(weights_transposed.shape)
+    prediction = x_test @ weights
+    # pprint(prediction)
+    # pprint(prediction.shape)
 
     # TODO: Compute root mean square error on the test set predictions
-    rmse = None
+    rmse = mean_squared_error(y_test, prediction, multioutput='uniform_average', squared=False)
 
     return rmse
 
